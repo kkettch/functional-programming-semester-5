1b563a0f603664f7799efa4838ec9c2a8177ae60
Mariia Chmurova <kkettch@gmail.com>
2024-10-07 20:56:46 +0300

    Add files via upload

---
+++ lab1/src/task21.fs
@@ -0,0 +1,77 @@
+﻿module AmicableNumbers
+
+// 1.1. Хвостовая рекурсия для нахождения суммы делителей 
+
+let d1 n =
+    let rec aux acc i =
+        if i >= n then acc
+        else if n % i = 0 then aux (acc + i) (i + 1)
+        else aux acc (i + 1)
+    aux 0 1
+
+
+// 1.2. Рекурсия для нахождения "дружественных" чисел меньше 10000
+
+let rec findAmicableNumbers n limit acc =
+    if n >= limit then acc
+    else
+        let b = d1 n
+        if b < limit && b <> n && d1 b = n then
+            findAmicableNumbers (n + 1) limit (n :: acc)
+        else
+            findAmicableNumbers (n + 1) limit acc
+
+let amicableSumRec = List.sum (findAmicableNumbers 1 10000 [])
+
+// 2. Модульная реализация
+
+let properDivisors n = 
+    [1 .. n/2] 
+    |> List.filter (fun x -> n % x = 0)
+
+let d2 n = List.sum (properDivisors n)
+
+let findAmicableNumbersModular limit =
+    [1 .. limit-1]
+    |> List.filter (fun a ->
+        let b = d2 a
+        b < limit && b <> a && d2 b = a
+    )
+
+let amicableSumModular = List.sum (findAmicableNumbersModular 10000)
+
+// 3. Генерация пар при помощи map
+
+let amicablePairs limit =
+    [1 .. limit-1]
+    |> List.map (fun a -> (a, d2 a))
+    |> List.filter (fun (a, b) -> b < limit && b <> a && d2 b = a)
+
+let amicableNumbers = amicablePairs 10000 |> List.map fst
+let amicableSumMap = List.sum amicableNumbers
+
+// 4. Специальный синтаксис для циклов
+
+let findAmicableNumbersLoop limit =
+    let amicableNumbers = ref []
+    for n in 1 .. limit - 1 do
+        let b = d2 n
+        if b < limit && b <> n && d2 b = n then
+            amicableNumbers := n :: !amicableNumbers
+    List.sum !amicableNumbers
+
+let amicableSumLoop = findAmicableNumbersLoop 10000
+
+// 5. Ленивые коллекции 
+
+let amicableNumbersUnder limit =
+    seq {
+        for n in 1 .. limit - 1 do
+            let b = d1 n
+            if b < limit && b <> n && d1 b = n then
+                yield n
+    }
+
+let amicableSumLazy = 
+    amicableNumbersUnder 10000 
+    |> Seq.sum
---
+++ lab1/src/task8.fs
@@ -0,0 +1,57 @@
+﻿module MaxProduct
+
+// 3. Преобразуем число в список цифр, используя генерацию последовательности при помощи отображения (map)
+
+let stringToIntList str =
+    str 
+    |> Seq.map (string >> int) 
+    |> Seq.toList
+
+// 1.1. Нахождение решения с помощью хвостовой рекурсии
+
+let rec findUsingTailRecursion numbersList maxProd i =
+    if i > (List.length numbersList - 13) then maxProd
+    else 
+        let currentDigits = 
+            List.skip i numbersList 
+            |> List.take 13
+        let product = List.fold (*) 1 currentDigits
+        let newMaxProd = if product > maxProd then product else maxProd
+        findUsingTailRecursion numbersList newMaxProd (i + 1)
+
+let findMaxProductTailRecursion str =
+    let numbersList = stringToIntList str
+    findUsingTailRecursion numbersList 0 0 
+
+// 1.1. Рекурсия
+
+let rec findUsingRecursion numbersList =
+    match numbersList with
+    | [] -> 0
+    | _ when List.length numbersList < 13 -> 0
+    | _ ->
+        let product = 
+            List.take 13 numbersList 
+            |> List.fold (*) 1
+        max product (findUsingRecursion (List.tail numbersList))
+
+let findMaxProductRecursion str =
+    let numbersList = stringToIntList str
+    findUsingRecursion numbersList
+
+// 4. Модульная реализация с разделением на функции и использование List.map и других функций высших порядкой:
+
+let getSubsequences numbersList = 
+    numbersList 
+    |> List.windowed 13 
+
+let productOfDigits numbersList =
+    List.fold (*) 1 numbersList
+
+let findMaxProductModule str =
+    str
+    |> stringToIntList
+    |> getSubsequences
+    |> List.map productOfDigits
+    |> List.max
+
