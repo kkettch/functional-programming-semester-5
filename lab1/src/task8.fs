dbfdf8c153e8549110fc4de63a4ab644a963afbf
Mariia Chmurova <kkettch@gmail.com>
2024-10-08 12:12:06 +0300

    Update task8.fs

--- lab1/src/task8.fs
+++ lab1/src/task8.fs
@@ -1,13 +1,11 @@
-﻿module MaxProduct
+module MaxProduct
 
-// 3. Преобразуем число в список цифр, используя генерацию последовательности при помощи отображения (map)
+open System.Numerics
 
 let stringToIntList str =
     str 
-    |> Seq.map (string >> int) 
+    |> Seq.map (fun ch -> bigint (int (string ch)))
     |> Seq.toList
-
-// 1.1. Нахождение решения с помощью хвостовой рекурсии
 
 let rec findUsingTailRecursion numbersList maxProd i =
     if i > (List.length numbersList - 13) then maxProd
@@ -15,38 +13,34 @@ let rec findUsingTailRecursion numbersList maxProd i =
         let currentDigits = 
             List.skip i numbersList 
             |> List.take 13
-        let product = List.fold (*) 1 currentDigits
+        let product = List.fold (*) 1I currentDigits
         let newMaxProd = if product > maxProd then product else maxProd
         findUsingTailRecursion numbersList newMaxProd (i + 1)
 
 let findMaxProductTailRecursion str =
     let numbersList = stringToIntList str
-    findUsingTailRecursion numbersList 0 0 
-
-// 1.1. Рекурсия
+    findUsingTailRecursion numbersList 0I 0 
 
 let rec findUsingRecursion numbersList =
     match numbersList with
-    | [] -> 0
-    | _ when List.length numbersList < 13 -> 0
+    | [] -> 0I
+    | _ when List.length numbersList < 13 -> 0I
     | _ ->
         let product = 
             List.take 13 numbersList 
-            |> List.fold (*) 1
+            |> List.fold (*) 1I
         max product (findUsingRecursion (List.tail numbersList))
 
 let findMaxProductRecursion str =
     let numbersList = stringToIntList str
     findUsingRecursion numbersList
 
-// 4. Модульная реализация с разделением на функции и использование List.map и других функций высших порядкой:
-
 let getSubsequences numbersList = 
     numbersList 
     |> List.windowed 13 
 
 let productOfDigits numbersList =
-    List.fold (*) 1 numbersList
+    List.fold (*) 1I numbersList
 
 let findMaxProductModule str =
     str
@@ -54,4 +48,3 @@ let findMaxProductModule str =
     |> getSubsequences
     |> List.map productOfDigits
     |> List.max
-
