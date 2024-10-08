dfd6bc7047261bc32b5a4c9c44425e995e428e27
Mariia Chmurova <kkettch@gmail.com>
2024-10-08 16:40:42 +0300

    Update task21.fs

--- lab1/src/task21.fs
+++ lab1/src/task21.fs
@@ -1,77 +1,56 @@
-﻿module AmicableNumbers
+module AmicableNumbers
 
-// 1.1. Хвостовая рекурсия для нахождения суммы делителей 
-
-let d1 n =
-    let rec aux acc i =
+let d n =
+    let rec findSum acc i =
         if i >= n then acc
-        else if n % i = 0 then aux (acc + i) (i + 1)
-        else aux acc (i + 1)
-    aux 0 1
+        elif n % i = 0 then findSum (acc + i) (i + 1)
+        else findSum acc (i + 1)
+    findSum 0 1
 
-
-// 1.2. Рекурсия для нахождения "дружественных" чисел меньше 10000
-
-let rec findAmicableNumbers n limit acc =
+let rec getAmicableNumbersRecursion n limit acc =
     if n >= limit then acc
     else
-        let b = d1 n
-        if b < limit && b <> n && d1 b = n then
-            findAmicableNumbers (n + 1) limit (n :: acc)
-        else
-            findAmicableNumbers (n + 1) limit acc
+        let b = d n
+        if b <> n && d b = n && b < limit then
+            getAmicableNumbersRecursion (n + 1) limit (n :: acc)
+        else getAmicableNumbersRecursion (n + 1) limit acc
 
-let amicableSumRec = List.sum (findAmicableNumbers 1 10000 [])
+let amicableSumRecursion = List.sum (getAmicableNumbersRecursion 1 10000 [])
 
-// 2. Модульная реализация
-
-let properDivisors n = 
-    [1 .. n/2] 
-    |> List.filter (fun x -> n % x = 0)
-
-let d2 n = List.sum (properDivisors n)
-
-let findAmicableNumbersModular limit =
+let getAmicablePairs limit =
     [1 .. limit-1]
-    |> List.filter (fun a ->
-        let b = d2 a
-        b < limit && b <> a && d2 b = a
-    )
+    |> List.map (fun a -> (a, d a))
+    |> List.filter (fun (a, b) -> a <> b && d b = a && b < limit)
 
-let amicableSumModular = List.sum (findAmicableNumbersModular 10000)
+let getAmicableNumbersMap = 
+    getAmicablePairs 10000
+    |> List.map fst
 
-// 3. Генерация пар при помощи map
+let amicableSumMap = List.sum getAmicableNumbersMap
 
-let amicablePairs limit =
-    [1 .. limit-1]
-    |> List.map (fun a -> (a, d2 a))
-    |> List.filter (fun (a, b) -> b < limit && b <> a && d2 b = a)
-
-let amicableNumbers = amicablePairs 10000 |> List.map fst
-let amicableSumMap = List.sum amicableNumbers
-
-// 4. Специальный синтаксис для циклов
-
-let findAmicableNumbersLoop limit =
-    let amicableNumbers = ref []
-    for n in 1 .. limit - 1 do
-        let b = d2 n
-        if b < limit && b <> n && d2 b = n then
-            amicableNumbers := n :: !amicableNumbers
-    List.sum !amicableNumbers
-
-let amicableSumLoop = findAmicableNumbersLoop 10000
-
-// 5. Ленивые коллекции 
-
-let amicableNumbersUnder limit =
+let getAmicableNumbersLazy limit =
     seq {
-        for n in 1 .. limit - 1 do
-            let b = d1 n
-            if b < limit && b <> n && d1 b = n then
+        for n in 1 .. limit-1 do
+            let b = d n
+            if b <> n && d b = n && b < limit then
                 yield n
     }
 
-let amicableSumLazy = 
-    amicableNumbersUnder 10000 
-    |> Seq.sum
+let amicableSumLazy = Seq.sum (getAmicableNumbersLazy 10000)
+
+let sumDivisors n =
+    [1 .. n/2]
+    |> List.filter (fun x -> n % x = 0)
+    |> List.sum
+
+let getAmicableNumberFilter limit =
+    [1 .. limit - 1]
+    |> List.filter 
+        (fun n -> 
+            let b = sumDivisors n
+            b <> n && sumDivisors b = n && b < limit
+        )
+
+let amicableSumFilter = List.sum (getAmicableNumberFilter 10000)
+
+
