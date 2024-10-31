c551f4486c82e7bd537bd53dffbae2c6acbc32eb
Mariia Chmurova <kkettch@gmail.com>
2024-10-31 14:29:16 +0300

    Update Rb_bag.fs

--- lab2/src/Rb_bag.fs
+++ lab2/src/Rb_bag.fs
@@ -1,135 +1,157 @@
 module RbBag
 
-type Color = Red | Black
+type Color = 
+    | Red
+    | Black
 
 type 'a Tree = 
     | Empty
     | Node of 'a TreeNode
-and 'a TreeNode = { value : 'a; color : Color; left : 'a Tree; right : 'a Tree }
+and 'a TreeNode = {
+    value : 'a; 
+    color : Color; 
+    left : 'a Tree; 
+    right : 'a Tree;
+}
 
-let empty : 'a Tree = Empty
+let empty = Empty
 
-let rec isMember (t : 'a Tree) (v : 'a) : bool =
-    match t with
+let rec isMember tree value =
+    match tree with
     | Empty -> false
-    | Node { value = v'; color = _; left = l; right = r } ->
-        if v < v' then isMember l v
-        else if v > v' then isMember r v
+    | Node {value = treeValue; color = _; left = left; right = right} ->
+        if value < treeValue then isMember left value
+        else if value > treeValue then isMember right value
         else true
+        
+let balance color x a b =
+    match (color, x, a, b) with
+    | (Black, z, // LL imbalance
+        Node {
+            value = y; 
+            color = Red; 
+            left = Node {value = x; color = Red; left = a; right = b}; 
+            right = c
+        }, d)     
+    | (Black, z, // LR imbalance
+        Node {
+            value = x; 
+            color = Red; 
+            left = a; 
+            right = Node {value = y; color = Red; left = b; right = c}; 
+        }, d)    
+    | (Black, x, a, // RL imbalance
+        Node { 
+            value = z; 
+            color = Red; 
+            left = Node {value = y; color = Red; left = b; right = c}; 
+            right = d; 
+        })    
+    | (Black, x, a, // RR imbalance
+        Node { 
+            value = y; 
+            color = Red; 
+            left = b; 
+            right = Node {value = z; color = Red; left = c; right = d}; 
+        }) ->
+        Node {  
+            value = y; 
+            color = Red; 
+            left = Node {value = x; color = Black; left = a; right = b}; 
+            right = Node {value = z; color = Black; left = c; right = d}
+        }
+    | _ -> Node {value = x; color = color; left = a; right = b}
 
-let insert (x : 'a) (t : 'a Tree) : 'a Tree =
-    let makeBlack = function
-        | Node { value = y; color = _; left = a; right = b} -> Node { value = y; color = Black; left = a; right = b }
+let insert x tree =
+    let makeBlack = 
+        function
+        | Node {value = y; color = _; left = left; right = right} -> Node {value = y; color = Black; left = left; right = right}
         | Empty -> failwith "Unexpected case: tree is empty"
 
-    let rec balance (color : Color) (a : 'a Tree) (x : 'a) (b : 'a Tree) =
-        match (color, a, x, b) with
-        | (Black, Node { value = y; color = Red; left = Node { value = x; color = Red; left = a; right = b }; right = c}, z, d)
-        | (Black, Node { value = x; color = Red; left = a; right = Node { value = y; color = Red; left = b; right = c }; }, z, d)
-        | (Black, a, x, Node { value = z; color = Red; left = Node { value = y; color = Red; left = b; right = c }; right = d; })
-        | (Black, a, x, Node { value = y; color = Red; left = b; right = Node { value = z; color = Red; left = c; right = d }; }) ->
-            Node {  value = y; color = Red; 
-                    left = Node {value = x; color = Black; left = a; right = b}; 
-                    right = Node {value = z; color = Black; left = c; right = d}
-                    }
-        | _ -> Node { value = x; color = color; left = a; right = b }
+    let rec insertOnCondition tree =
+        match tree with
+        | Empty -> Node {value = x; color = Red; left = Empty; right = Empty}
+        | Node {value = y; color = color; left = a; right = b} ->
+            if x < y then balance color y (insertOnCondition a) b
+            else balance color y a (insertOnCondition b)
 
-    let rec ins t =
-        match t with
-        | Empty -> Node { value = x; color = Red; left = Empty; right = Empty }
-        | Node { value = y; color = color; left = a; right = b } ->
-            if x < y then balance color (ins a) y b
-            else if x >= y then balance color a y (ins b)
-            else Node { value = y; color = color; left = a; right = b }
+    makeBlack (insertOnCondition tree)
 
-    makeBlack (ins t)
-
-let insertMany (xs : 'a seq) (t : 'a Tree) : 'a Tree =
+let insertMany xs tree =
     let switch f = fun y x -> f x y
-    xs |> Seq.fold (switch insert) t
+    xs 
+    |> Seq.fold (switch insert) tree
 
-let rec delete (x : 'a) (t : 'a Tree) : 'a Tree =
-    let rec balance (color : Color) (a : 'a Tree) (x : 'a) (b : 'a Tree) =
-        match color, a, x, b with
-        | (Black, Node { value = y; color = Red; left = Node { value = x; color = Red; left = a; right = b }; right = c }, z, d)
-        | (Black, Node { value = x; color = Red; left = a; right = Node { value = y; color = Red; left = b; right = c } }, z, d)
-        | (Black, a, x, Node { value = z; color = Red; left = Node { value = y; color = Red; left = b; right = c }; right = d })
-        | (Black, a, x, Node { value = y; color = Red; left = b; right = Node { value = z; color = Red; left = c; right = d } }) ->
-            Node { value = y; color = Red; left = Node { value = x; color = Black; left = a; right = b }; right = Node { value = z; color = Black; left = c; right = d } }
-        | _ -> Node { value = x; color = color; left = a; right = b }
-
-    let rec findMin t =
-        match t with
+let rec delete x tree=
+    let rec findMin tree =
+        match tree with
+        | Node {color = _; value = value; left = Empty; right = _} -> value
+        | Node {color = _; value = _; left = left; right = _} -> findMin left
         | Empty -> failwith "Unexpected case: tree is empty"
-        | Node { value = v; left = Empty; right = _ } -> v
-        | Node { left = l } -> findMin l
-
-    let rec deleteMin t =
-        match t with
+        
+    let rec deleteMin tree =
+        match tree with
+        | Node {value = _; color = _; left = Empty; right = right} -> right
+        | Node {value = value; color = color; left = left; right = right} ->
+            balance color value (deleteMin left) right
         | Empty -> failwith "Unexpected case: tree is empty"
-        | Node { value = v; color = c; left = Empty; right = r } -> r
-        | Node { value = v; color = c; left = l; right = r } ->
-            balance c (deleteMin l) v r
 
-    let rec deleteNode t =
-        match t with
+    let rec deleteNode tree =
+        match tree with
+        | Node {value = value; color = color; left = left; right = right} ->
+            if x < value then balance color value (deleteNode left) right
+            else if x > value then balance color value left (deleteNode right)
+            else match right with
+                 | Empty -> left
+                 | _ -> let minValue = findMin right
+                        balance color minValue left (deleteMin right)
         | Empty -> Empty
-        | Node { value = v; color = c; left = l; right = r } ->
-            if x < v then balance c (deleteNode l) v r
-            else if x > v then balance c l v (deleteNode r)
-            else match r with
-                 | Empty -> l
-                 | _ -> let m = findMin r
-                        balance c l m (deleteMin r)
 
-    match t with
+    match tree with
     | Empty -> Empty
-    | Node _ -> deleteNode t
+    | Node _ -> deleteNode tree
 
-let rec map (f : 'a -> 'b) (t : 'a Tree) : 'b Tree =
-    match t with
+let rec map mapFunction tree =
+    match tree with
     | Empty -> Empty
-    | Node { value = v; color = c; left = l; right = r } ->
-        Node { value = f v; color = c; left = map f l; right = map f r }
+    | Node {value = value; color = color; left = left; right = right} ->
+        Node { 
+            value = mapFunction value; 
+            color = color; 
+            left = map mapFunction left; 
+            right = map mapFunction right 
+        }
 
-let rec foldLeft (f : 'b -> 'a -> 'b) (acc : 'b) (t : 'a Tree) : 'b =
-    match t with
+let rec foldLeft f acc tree =
+    match tree with
     | Empty -> acc
-    | Node { value = v; color = _; left = l; right = r } ->
-        let acc' = foldLeft f acc l
-        let acc'' = f acc' v
-        foldLeft f acc'' r
+    | Node {value = value; color = _; left = left; right = right} ->
+        let accLeftSubtree = foldLeft f acc left
+        let accCounted = f accLeftSubtree value
+        foldLeft f accCounted right
 
-let rec foldRight (f : 'a -> 'b -> 'b) (t : 'a Tree) (acc : 'b) : 'b =
-    match t with
+let rec foldRight f tree acc =
+    match tree with
     | Empty -> acc
-    | Node { value = v; color = _; left = l; right = r } ->
-        let acc' = foldRight f r acc
-        let acc'' = f v acc'
-        foldRight f l acc''
+    | Node {value = value; color = _; left = left; right = right} ->
+        let accRightSubtree = foldRight f right acc
+        let accCounted = f accRightSubtree value 
+        foldRight f left accCounted
 
-let rec filter (pred: 'a -> bool) (t: 'a Tree) : 'a Tree =
-    match t with
+let combineTrees left right =
+    match left, right with
+    | Empty, _ -> right
+    | _, Empty -> left
+    | Node {value = v; color = c; left = _; right = _} as node, _ ->
+        Node {value = v; color = c; left = left; right = right}
+        
+let rec filter pred tree =
+    match tree with
     | Empty -> Empty
-    | Node { value = v; color = c; left = l; right = r } ->
-        let leftFiltered = filter pred l
-        let rightFiltered = filter pred r
-        if pred v then
-            Node { value = v; color = c; left = leftFiltered; right = rightFiltered }
+    | Node {value = value; color = color; left = left; right = right} ->
+        let leftFiltered = filter pred left
+        let rightFiltered = filter pred right
+        if pred value then
+            Node {value = value; color = color; left = leftFiltered; right = rightFiltered}
         else
             combineTrees leftFiltered rightFiltered
-
-and combineTrees (l: 'a Tree) (r: 'a Tree) : 'a Tree =
-    match l, r with
-    | Empty, _ -> r
-    | _, Empty -> l
-    | Node { value = v; color = c; left = _; right = _ } as node, _ ->
-        Node { value = v; color = c; left = l; right = r }
-
-let rec contains (t: 'a Tree) (v: 'a) : bool =
-    match t with
-    | Empty -> false
-    | Node { value = v'; color = _; left = l; right = r } ->
-        if v < v' then contains l v
-        else if v > v' then contains r v
-        else true
