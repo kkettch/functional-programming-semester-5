1fc9583b911534db9cf76ba0b896e5e248a4f020
Mariia Chmurova <kkettch@gmail.com>
2024-11-05 10:51:57 +0300

    Update PropertyBased.fs

--- lab2/test/PropertyBased.fs
+++ lab2/test/PropertyBased.fs
@@ -4,50 +4,58 @@ open FsCheck.Xunit
 open Xunit
 open RbBag
 
-let combineTreesMonoid (t1: 'a Tree) (t2: 'a Tree) : 'a Tree =
-    insertMany (foldLeft (fun acc x -> x :: acc) [] t1) t2
+let rec allElementsSatisfy tree pred =
+    match tree with
+    | Empty -> true
+    | Node {value = v; left = l; right = r} ->
+        pred v && allElementsSatisfy l pred && allElementsSatisfy r pred
+
+let rec allElementsInOtherTree tree otherTree =
+    match tree with
+    | Empty -> true
+    | Node {value = v; left = l; right = r} ->
+        isMember otherTree v && allElementsInOtherTree l otherTree && allElementsInOtherTree r otherTree
 
 [<Property>]
-let ``Combining two trees gives a valid tree`` (xs: int list) (ys: int list) =
+let ``Empty tree is neutral element for insertMany`` (xs: int list) =
+    let tree = insertMany xs empty
+    let combinedWithEmptyLeft = insertMany [] tree
+    let combinedWithEmptyRight = insertMany xs empty
+    combinedWithEmptyLeft = tree && combinedWithEmptyRight = tree
+
+[<Property>]
+let ``Tree combination is associative`` (xs: int list) (ys: int list) (zs: int list) =
     let tree1 = insertMany xs empty
     let tree2 = insertMany ys empty
-    let combined = combineTreesMonoid tree1 tree2
-    List.forall (fun x -> isMember combined x) (xs @ ys)
-
-[<Property>]
-let ``Combining two empty trees results in an empty tree`` () =
-    let emptyTree1 = empty
-    let emptyTree2 = empty
-    let combinedEmptyTrees = combineTreesMonoid emptyTree1 emptyTree2
-    combinedEmptyTrees = Empty
-
-[<Property>]
-let ``Filter should only retain elements satisfying predicate`` (xs: int list) =
-    let tree = insertMany xs empty
-    let pred x = x > 0
-    let filteredTree = filter pred tree
-    let allRetained = List.forall (fun x -> pred x = (isMember filteredTree x)) xs
-    allRetained
+    let tree3 = insertMany zs empty
+    let combined1 = insertMany ys (insertMany xs tree3)
+    let combined2 = insertMany xs (insertMany ys tree3)
+    allElementsInOtherTree combined1 combined2 && allElementsInOtherTree combined2 combined1
 
 [<Property>]
 let ``Map preserves elements in tree`` (xs: int list) =
     let tree = insertMany xs empty
     let mappedTree = map ((+) 1) tree
-    let allMapped = List.forall (fun x -> isMember mappedTree (x + 1)) xs
-    allMapped
-
-[<Property>]
-let ``All elements in int list are members after insertion`` (xs: int list) =
-    let tree = insertMany xs empty
-    xs |> List.forall (fun x -> isMember tree x)
-
-[<Property>]
-let ``Empty tree is neutral element for insertMany`` (xs: int list) =
-    let t = insertMany xs empty
-    Assert.Equal(t, insertMany [] t)
+    let rec checkMapped tree =
+        match tree with
+        | Empty -> true
+        | Node {value = v; left = l; right = r} ->
+            isMember mappedTree (v + 1) && checkMapped l && checkMapped r
+    checkMapped tree
 
 [<Property>]
 let ``InsertMany preserves elements`` (xs: int list) =
     let t = insertMany xs empty
-    for x in xs do
-        Assert.True(isMember t x)
+    let rec checkElementsInTree tree elements =
+        match elements with
+        | [] -> true
+        | x::xs -> isMember tree x && checkElementsInTree tree xs
+    checkElementsInTree t xs
+
+[<Property>]
+let ``Filter should only retain elements satisfying predicate`` (xs: int list) =
+    let tree = insertMany xs empty
+    let pred x = x > 3
+    let filteredTree = filter pred tree
+    allElementsSatisfy filteredTree pred
+
