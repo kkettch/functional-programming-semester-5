6ac44f5d238f48e1858991103d91ed64c513374d
kkettch <maria.chmurova884@gmail.com>
2024-12-19 13:21:02 +0300

    PBT for monoid

--- lab2/test/PropertyBased.fs
+++ lab2/test/PropertyBased.fs
@@ -1,44 +1,41 @@
-module RbBagTests
+module CustomTreeGen
 
 open FsCheck.Xunit
-open Xunit
+open FsCheck
 open RbBag
 
-let rec allElementsSatisfy tree pred =
-    match tree with
-    | Empty -> true
-    | Node {value = v; left = l; right = r} ->
-        pred v && allElementsSatisfy l pred && allElementsSatisfy r pred
+open FsCheck
 
-let rec allElementsInOtherTree tree otherTree =
-    match tree with
-    | Empty -> true
-    | Node {value = v; left = l; right = r} ->
-        isMember otherTree v && allElementsInOtherTree l otherTree && allElementsInOtherTree r otherTree
+type ArbitraryTree<'T when 'T : comparison>() =
+    static member Tree =
+        Arb.fromGen <| 
+            gen {
+                let! size = Gen.choose (10, 50)
+                let! values = Gen.listOfLength size (Arb.generate<'T>)
+                return List.fold (fun acc n -> insert n acc) Empty values
+            }
 
-[<Property>]
-let ``Empty tree is neutral element for insertMany`` (xs: int list) =
-    let tree = insertMany xs empty
-    let combinedWithEmptyLeft = insertMany [] tree
-    let combinedWithEmptyRight = insertMany xs empty
+[<Property(Arbitrary = [| typeof<ArbitraryTree<int>> |])>]
+let prop_emptyMerge (bag: Tree<int>) =
+    equal bag (merge bag Empty)
+
+[<Property(Arbitrary = [| typeof<ArbitraryTree<int>> |])>]
+let prop_associativity (bag1: Tree<int>) (bag2: Tree<int>) (bag3: Tree<int>) =
+    let mergedBag = merge bag1 (merge bag2 bag3)
+    let mergedBag2 = merge (merge bag1 bag2) bag3
+
+    equal mergedBag mergedBag2
+
+
+[<Property(Arbitrary = [| typeof<ArbitraryTree<int>> |])>]
+let ``Empty tree is neutral element for merge`` (tree: Tree<int>) =
+    let combinedWithEmptyLeft = merge empty tree
+    let combinedWithEmptyRight = merge tree empty
+
     combinedWithEmptyLeft = tree && combinedWithEmptyRight = tree
 
-[<Property>]
-let ``Tree combination is associative`` (xs: int list) (ys: int list) (zs: int list) =
-    let tree1 = insertMany xs empty
-    let tree2 = insertMany ys empty
-    let tree3 = insertMany zs empty
-    let combined1 = insertMany zs (insertMany xs tree2) // ((xs + ys) + zs)
-    let combined2 = insertMany xs (insertMany ys tree3) // (xs + (ys + zs))
-    let combined3 = insertMany ys (insertMany zs tree1) // ((zs + xs) + ys)
-
-    allElementsInOtherTree combined1 combined2 && 
-    allElementsInOtherTree combined2 combined3 &&
-    allElementsInOtherTree combined1 combined3
-
-[<Property>]
-let ``Map preserves elements in tree`` (xs: int list) =
-    let tree = insertMany xs empty
+[<Property(Arbitrary = [| typeof<ArbitraryTree<int>> |])>]
+let ``Map preserves elements in tree`` (tree: Tree<int>) =
     let mappedTree = map ((+) 1) tree
     let rec checkMapped tree =
         match tree with
@@ -47,19 +44,22 @@ let ``Map preserves elements in tree`` (xs: int list) =
             isMember mappedTree (v + 1) && checkMapped l && checkMapped r
     checkMapped tree
 
-[<Property>]
-let ``InsertMany preserves elements`` (xs: int list) =
-    let t = insertMany xs empty
-    let rec checkElementsInTree tree elements =
-        match elements with
-        | [] -> true
-        | x::xs -> isMember tree x && checkElementsInTree tree xs
-    checkElementsInTree t xs
 
-[<Property>]
-let ``Filter should only retain elements satisfying predicate`` (xs: int list) =
-    let tree = insertMany xs empty
+[<Property(Arbitrary = [| typeof<ArbitraryTree<int>> |])>]
+let ``Filter should only retain elements satisfying predicate`` (tree: Tree<int>) =
     let pred x = x > 3
     let filteredTree = filter pred tree
+
     allElementsSatisfy filteredTree pred
 
+
+(*
+    Покраска - дерево удовлетворяет всем правилам
+*)
+[<Property(Arbitrary = [| typeof<ArbitraryTree<int>> |])>]
+let ``Tree coloring is correct`` (tree: Tree<int>) =
+    let bh = blackDeepth tree
+
+    rootIsBlack tree && 
+    noRRImbalance tree && 
+    sameBlackDeepth tree bh
