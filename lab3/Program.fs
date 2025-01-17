84c90629296a00fbb90bb965e7c6102bd3a522ec
Mariia Chmurova <kkettch@gmail.com>
2025-01-17 13:03:38 +0300

    Update Program.fs

--- lab3/Program.fs
+++ lab3/Program.fs
@@ -1,13 +1,21 @@
-﻿module Program
+module Program
 
 open System
-
 open InputHandler
 open ProcessInput
 open OutputHandler
 
 [<EntryPoint>]
 let main (args: string array) =
-    let points = handleInput Console.In
-    processInput points
+    let rec getStep () =
+        printfn "step value:"
+        match Console.ReadLine() |> System.Double.TryParse with
+        | (true, step) -> step 
+        | (false, _) ->
+            printfn "error: invalid input. correct input: number"
+            getStep () 
+
+    let step = getStep ()
+    let points = handleInput Console.In 
+    processInput points step |> handleOutput
     0
